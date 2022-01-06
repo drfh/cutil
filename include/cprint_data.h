def0afbeb5fdd768b715243aba6bb93dbb033e8d
David R. F. Harris <DavidRFHarris@gmail.com>
2022-01-06 07:29:42 -0500

    Update cprint_data.h

--- include/cprint_data.h
+++ include/cprint_data.h
@@ -1,8 +1,11 @@
-#include <stdbool.h>
-#include <stdint.h>
-
+/**
+ * @file cprint_data.h
+ */
 #ifndef _CPRINT_H_
 #define	_CPRINT_H_
+
+#include <stdbool.h>
+#include <stdint.h>
 
 
 void cprint_bits8(uint8_t x);
